a548735696225f6bf702cac5f87376522e792a84
aungmyohtet <aungmyohtet@outlook.com>
2017-07-11 16:42:05 +0630

    fixed errors got from merging

--- src/main/java/com/aungmyohtet/pm/web/OrganizationController.java
+++ src/main/java/com/aungmyohtet/pm/web/OrganizationController.java
@@ -1,6 +1,5 @@
 package com.aungmyohtet.pm.web;
 
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -39,149 +38,6 @@ import com.aungmyohtet.pm.service.UserService;
 @Controller
 public class OrganizationController {
 
-	@Autowired
-	private UserService userService;
-
-	@Autowired
-	private ProjectService projectService;
-
-	@Autowired
-	private OrganizationMemberService organizationMemberService;
-
-	@Autowired
-	private OrganizationService organizationService;
-
-	@ModelAttribute("module")
-	String module() {
-		return "home";
-	}
-
-	public void setOrganizationService(OrganizationService organizationService) {
-		this.organizationService = organizationService;
-	}
-
-	@RequestMapping(value = "/organizations/new", method = RequestMethod.GET)
-	public String showOrganizationForm(Model model) {
-		model.addAttribute("organization", new Organization());
-		return "organizationForm";
-	}
-
-	@RequestMapping(value = "/organizations/new", method = RequestMethod.POST)
-	public String addOrganization(@Validated @ModelAttribute Organization organization, BindingResult result,
-			Model model) {
-		if (result.hasErrors()) {
-			return "organizationForm";
-		}
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName(); // we used email in user details service
-		organizationService.addByUser(organization, email);
-		return "redirect:/organizations";
-	}
-
-	@RequestMapping(value = "/json/{organizationName}/members", method = RequestMethod.GET)
-	@ResponseBody
-	public List<UserDto> getOrganizationMembers(@PathVariable("organizationName") String organizationName,
-			Model model) {
-		/*
-		 * List<User> users = new ArrayList<>(); List<OrganizationMember>
-		 * members = organizationService.findMembersByOrganization(id); for
-		 * (OrganizationMember member : members) { users.add(member.getUser());
-		 * }
-		 */
-		List<User> users = userService.findMembersOfOrganization(organizationName);
-		return users.stream().map(user -> userService.converToDto(user)).collect(Collectors.toList());
-	}
-
-	@RequestMapping(value = "/{organizationName}/members", method = RequestMethod.GET)
-	public String showOrganizationMembers(@PathVariable("organizationName") String organizationName, Model model) {
-		List<User> users = userService.findMembersOfOrganization(organizationName);
-		List<UserDto> userDtos = users.stream().map(user -> userService.converToDto(user)).collect(Collectors.toList());
-		model.addAttribute("organizationName", organizationName);
-		model.addAttribute("members", userDtos);
-		model.addAttribute("module", "members");
-		return "organizationMembers";
-	}
-
-	@RequestMapping(value = "/{organizationName}/members/new", method = RequestMethod.GET)
-	public String showOrganizationMemberForm(@PathVariable("organizationName") String organizationName, Model model) {
-		model.addAttribute("user", new User());
-		model.addAttribute("organizationName", organizationName);
-		model.addAttribute("module", "members");
-		return "organizationMemberForm";
-	}
-
-	@RequestMapping(value = "/{organizationName}/members/new", method = RequestMethod.POST)
-	public String addMemberToOrganization(@PathVariable("organizationName") String organizationName, Model model,
-			@ModelAttribute User user) {
-		System.out.println("user email is " + user.getEmail());
-		organizationMemberService.addMemberToOrganization(user.getEmail(), organizationName);
-		return "redirect:/" + organizationName + "/members";
-	}
-
-	@RequestMapping(value = "/json/organizations", method = RequestMethod.GET)
-	@ResponseBody
-	public List<OrganizationDto> list(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName();
-		List<Organization> organizations = userService.findOrganizationsByUser(email);
-		return organizations.stream().map(organization -> organizationService.convertToDto(organization))
-				.collect(Collectors.toList());
-	}
-
-	@RequestMapping(value = "/organizations", method = RequestMethod.GET)
-	public String showOrganizations(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName();
-		model.addAttribute("ownedOrganizations", userService.findOrganizationsCreatedByUser(email));
-		model.addAttribute("involvedOrganizations", userService.findOrganizationsInvolvingUser(email));
-		return "organizations";
-	}
-
-	@RequestMapping(value = "/{organizationName}", method = RequestMethod.GET)
-	public String showOrganizationName(@PathVariable("organizationName") String organizationName, Model model) {
-		model.addAttribute("page_title", organizationName);
-		return "organizationHome";
-	}
-
-	@RequestMapping(value = "/organizations/my", method = RequestMethod.GET)
-	@ResponseBody
-	public List<OrganizationDto> getOwnedOrganizations(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName();
-		List<Organization> organizations = userService.findOrganizationsCreatedByUser(email);
-		return organizations.stream().map(organization -> organizationService.convertToDto(organization))
-				.collect(Collectors.toList());
-	}
-
-	@RequestMapping(value = "/organizations/in", method = RequestMethod.GET)
-	@ResponseBody
-	public List<OrganizationDto> getInvolvedOrganizations(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName();
-		List<Organization> organizations = userService.findOrganizationsInvolvingUser(email);
-		return organizations.stream().map(organization -> organizationService.convertToDto(organization))
-				.collect(Collectors.toList());
-	}
-
-	@RequestMapping(value = "/json/{organizationName}/projects", method = RequestMethod.GET)
-	@ResponseBody
-	public List<ProjectDto> getOrganizationProjects(@PathVariable("organizationName") String organizationName,
-			Model model) {
-		List<Project> projects = organizationService.findProjectsByOrganization(organizationName);
-		return projects.stream().map(project -> projectService.converToDto(project)).collect(Collectors.toList());
-	}
-
-	@RequestMapping(value = "/{organizationName}/projects", method = RequestMethod.GET)
-	public String showOrganizationProjects(@PathVariable("organizationName") String organizationName, Model model) {
-		List<Project> projects = organizationService.findProjectsByOrganization(organizationName);
-		List<ProjectDto> projectDtos = projects.stream().map(project -> projectService.converToDto(project))
-				.collect(Collectors.toList());
-		model.addAttribute("organizationName", organizationName);
-		model.addAttribute("projects", projectDtos);
-		model.addAttribute("module", "projects");
-		return "projectList";
-	}
-
     @Autowired
     private UserService userService;
 
@@ -196,6 +52,11 @@ public class OrganizationController {
 
     @Autowired
     private ResourceService resourceService;
+
+    @ModelAttribute("module")
+    String module() {
+        return "home";
+    }
 
     public void setOrganizationService(OrganizationService organizationService) {
         this.organizationService = organizationService;
@@ -236,6 +97,7 @@ public class OrganizationController {
         List<UserDto> userDtos = users.stream().map(user -> userService.converToDto(user)).collect(Collectors.toList());
         model.addAttribute("organizationName", organizationName);
         model.addAttribute("members", userDtos);
+        model.addAttribute("module", "members");
         return "organizationMembers";
     }
 
