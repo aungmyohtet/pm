72b6b9805ee163b17276bedc2a70ff7ac0b36c48
aungmyohtet <aungmyohtet@outlook.com>
2017-07-25 23:02:55 +0630

    updated board and card controller

--- src/main/java/com/aungmyohtet/pm/web/update/UpdatedBoardController.java
+++ src/main/java/com/aungmyohtet/pm/web/update/UpdatedBoardController.java
@@ -5,27 +5,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.aungmyohtet.pm.entity.Board;
 import com.aungmyohtet.pm.entity.Card;
 import com.aungmyohtet.pm.entity.Organization;
-import com.aungmyohtet.pm.service.BoardService2;
-import com.aungmyohtet.pm.service.OrganizationService2;
+import com.aungmyohtet.pm.service.update.BoardService;
+import com.aungmyohtet.pm.service.update.OrganizationService;
 
 @Controller
 public class UpdatedBoardController {
 
     @Autowired
-    private BoardService2 boardService;
+    private BoardService boardService;
 
     @Autowired
-    private OrganizationService2 organizationService;
+    private OrganizationService organizationService;
 
     @GetMapping(value = "/u/{organizationName}/boards/{boardName}")
     private String showBoardMenu(Model model, @PathVariable("organizationName") String organizationName, @PathVariable("boardName") String boardName) {
-        return "redirect:/" + organizationName + "/boards/" + boardName + "/cards";
+        return "redirect:/u/" + organizationName + "/boards/" + boardName + "/cards";
     }
 
     @GetMapping(value = "/u/{organizationName}/boards/{boardName}/cards")
@@ -37,6 +41,23 @@ public class UpdatedBoardController {
         List<Card> cards = boardService.getCards(board);
         model.addAttribute("cards", cards);
         return "card/cards";
+    }
+
+    @GetMapping(value = "/u/{organizationName}/boards/new")
+    public String showBoardForm(Model model, @PathVariable("organizationName") String organizationName) {
+        model.addAttribute("board", new Board());
+        model.addAttribute("organizationName", organizationName);
+        return "board/form";
+    }
+
+    @PostMapping(value = "/u/{organizationName}/boards/new")
+    public String addBoard(@Validated @ModelAttribute Board board, BindingResult result, Model model, @PathVariable("organizationName") String organizationName) {
+        if (result.hasErrors()) {
+            return "board/form";
+        }
+        Organization organization = organizationService.findByName(organizationName);
+        organizationService.addBoard(organization, board);
+        return "redirect:/u/" + organizationName + "/boards";
     }
 
 }
